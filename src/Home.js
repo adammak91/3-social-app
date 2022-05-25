2059ec92673119d4874229f1015ea81d53f3e5f1
adammak91 <adam210125@o2.pl>
2022-05-25 15:48:38 +0200

    login

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Home.css';
 import axios from 'axios';
 import Alteposts from './Alteposts';
-import Nextpost from './Nextpost';
+
 
 
 class Home extends Component {
@@ -11,73 +11,61 @@ class Home extends Component {
         super(props);
 
         this.state = {
-          postList: []
-      };
+            postList: []
+        };
     }
 
 
     getPosts = () => {
-      let axiosConfig = {
-          headers: {
-              'Content-Type': 'application/json',
-              'Accept': 'application/json'
-          }
-      };
+        let axiosConfig = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        };
 
-      axios.post('https://akademia108.pl/api/social-app/post/latest',{},axiosConfig)
-      .then((res) => {
-          console.log(res.data);
-          this.setState({postList:res.data})
-      })
-  }
-
-
- 
- 
-  getNextPost = () => {
-      let axiosConfig = {
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-      }
-  }
-    axios.post('https://akademia108.pl/api/social-app/post/older-then', axiosConfig,  
-    { date: this.state.postList[this.state.postList.length - 1].created_at })
-    .then((res) => {
-        this.setState({postList: this.state.postList.concat(res.data)})
-    })
-    // .catch((err) => {
-    //     console.log("AXIOS ERROR: ", err);
-    // })
-}
-
-  componentDidMount(){
-      this.getPosts();
-  }
-
-
-
-  render() {
-      
-
-     
-      return (
-  
-          <div className="postsList">
-              <h3>Posts</h3>
-              <Alteposts posts={this.state.postList} />
-          </div>
-      ) 
-    
-      return (
-        <div className='nextposts'>
-            <button onClick={this.getNextPost}>Wiecej</button>
-        </div>
-      )
+        axios.post('https://akademia108.pl/api/social-app/post/latest', {}, axiosConfig)
+            .then((res) => {
+                console.log(res.data);
+                this.setState({ postList: res.data })
+            })
     }
-  }
 
-   
 
+
+    getNextPost = () => {
+        let axiosConfig = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }
+        axios.post('https://akademia108.pl/api/social-app/post/older-then',
+            { date: this.state.postList[this.state.postList.length - 1].created_at },
+            axiosConfig)
+            .then((res) => {
+                this.setState({ postList: this.state.postList.concat(res.data) })
+            })
+        // .catch((err) => {
+        //     console.log("AXIOS ERROR: ", err);
+        // })
+    }
+
+    componentDidMount() {
+        this.getPosts();
+    }
+
+    render() {
+
+        return (
+
+            <div className="postsList">
+                <h3>Posts</h3>
+                <Alteposts posts={this.state.postList} />
+                <button onClick={this.getNextPost}>Wiecej</button>
+            </div>
+        )
+    }
+}
 
 export default Home;
--- src/Login.js
+++ src/Login.js
@@ -1,23 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
+import './Login.css';
+import axios from "axios";
+
+class Login extends Component {
 
 
-const Login = () => {
+    hendleSubmit = (e) => {
+        e.preventDefault()
 
-    return(
-        <div>
-        <h3>Login</h3>
-         <form>
-             <div className="form-group">
-                   <label>Nazwe uzytkownika</label>
-                   <input type="text" minLength={4}></input>
-                   <label>Wpisz haslo</label>
-                   <input type="password"></input>
-                   <label>Zaloguj sie</label>
-                   <input type="submit" className="submit"></input>
-             </div>
-         </form>
-       </div> 
-    );
+
+        const user = {
+            "username": "adam",
+            "password": "1234",
+            "ttl": "3600"
+
+        }
+        console.log(user)
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+
+        axios.post('https://akademia108.pl/api/social-app/user/login',JSON.stringify(user),
+            { 'headers': headers })
+            .then((req) => {
+
+                localStorage.setItem('user', JSON.stringify(user));
+
+                console.log(req.data);})
+            
+            // .catch((error) => {
+            //     console.error(error);
+            // })
+    }
+    render() {
+
+        return (
+            <div>
+                <h3>Login</h3>
+                <form>
+                    <div className="form-group">
+                        <label className="LoginLabel">Nazwe uzytkownika</label>
+                        <input className="LoginInput" type="text" onChange={e => this.username = e.target.value} minLength={4}></input>
+
+                        <label className="LoginLabel">Wpisz haslo</label>
+                        <input className="LoginInput" type="password" onChange={e => this.password = e.target.value}></input>
+
+                        <div className="Loginbutton">
+                            <input type="submit" className="submit" value="Login"></input>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
 
 
