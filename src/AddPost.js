785b4e21af1943564f5de0c7b839abe8d1f7c87f
adammak91 <adam210125@o2.pl>
2022-06-01 16:43:42 +0200

    addpost

--- src/AddPost.js
+++ src/AddPost.js
@@ -2,43 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import { render } from "@testing-library/react";
 
-class PostAdd extends Component {
-  constructor() {
-    super();
 
-    this.state = {
-      newPost: '',
-    }
-  }
-
-  addPost = () => {
-    e.preventDefault();
-
-    let data = {
-      content: this.userAddPost
-    }
-
-    let axiosConfig = {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        Authorization: "Bearer " + this.props.user.jwt_token
-      }
-    };
-
-    axios.post(
-      'https://akademia108.pl/api/social-app/post/add', {data}, axiosConfig)
-      .then(req => {
-        let reqData = req.data;
-        console.log(reqData);
-        // this.setState({newPost: req.data})
-      })
-
-      // .catch(error => {
-      //   console.log(error.data.newPost);
-      //   this.setState(error.data) })
-     
-  }
 
 
   render() {
