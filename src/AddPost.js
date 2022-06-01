469e1098bad07e55966a32ddb026a66064d02e86
adammak91 <adam210125@o2.pl>
2022-06-01 17:12:37 +0200

    addPost

--- src/AddPost.js
+++ src/AddPost.js
@@ -4,19 +4,4 @@ import { render } from "@testing-library/react";
 
 
 
-
-  render() {
-    return (
-      <div className="add-post">
-        
-          <form className="form addPost" onSubmit={e => this.addPost(e)}>
-            <textarea placeholder="dodaj post" onChange={(e) => this.userAddPost(e)}></textarea>
-            <button type="submit" className="btn">Dodaj Post</button>
-          </form>
-        
-      </div>
-    )
-  }
-}
-
 export default AddPost;
